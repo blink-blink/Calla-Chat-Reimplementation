f9d13d59e8e1dde81073a9912e3068b7cd1af2fa
Your Name <gsamueljose@gmail.com>
2022-06-24 05:44:32 +0800

    added some trees

--- Scripts/PlayerCamera.gd
+++ Scripts/PlayerCamera.gd
@@ -4,5 +4,8 @@ onready var player = get_node("../Player")
 
 var acceleration = 7
 
+func _ready():
+	global_transform.origin = player.global_transform.origin
+
 func _physics_process(delta):
 	global_transform.origin = lerp(global_transform.origin, player.global_transform.origin, delta*acceleration)
