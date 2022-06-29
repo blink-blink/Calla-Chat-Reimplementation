6438554a9b617f2aa21319cf4f6155306f486ee0
walterlim <walterlim2001@yahoo.com>
2022-06-29 13:52:25 +0800

    Prop tiles now have navmesh

--- Scenes/Map1Navigation.gd
+++ Scenes/Map1Navigation.gd
@@ -1,9 +1,11 @@
 extends Navigation2D
 
-onready var Tiles = get_node("TileMap")
+
+onready var Props = get_node("../Props")
 
 func _ready():
 	trees()
+	tileuseall(Props)
 	$NavigationPolygonInstance.enabled = false
 	$NavigationPolygonInstance.enabled = true
 	
@@ -29,12 +31,18 @@ func tileuse(Tilemap, ID):
 	var used_tiles = Tilemap.get_used_cells_by_id(ID)
 	for tile in used_tiles:
 		var newpolygon = PoolVector2Array()
-		var polygon_offset = Tilemap.map_to_world(tile) - Vector2(Tilemap.get_cell_size()[0]/2, 0)
+		var polygon_offset = Tilemap.map_to_world(tile)
 		var tileregion = Tilemap.get_cell_autotile_coord(tile[0],tile[1])
 		var tiletransform = Tilemap.get_tileset().tile_get_shape_transform(ID, tileregion[0])
-		var polygon_bp = Tilemap.get_tileset().tile_get_shape(ID, tileregion[0].get_points())
+		var polygon_bp = Tilemap.get_tileset().tile_get_shape(ID, tileregion[0]).get_points()
 		for vertex in polygon_bp:
 			vertex += polygon_offset
 			newpolygon.append(tiletransform.xform(vertex))
-			
+		polygon.add_outline(newpolygon)
+	polygon.make_polygons_from_outlines()		
 	$NavigationPolygonInstance.set_navigation_polygon(polygon)
+	
+func tileuseall(Tilemap):
+	var IDs = Tilemap.get_tileset().get_tiles_ids()
+	for ID in IDs:
+		tileuse(Tilemap, ID)
