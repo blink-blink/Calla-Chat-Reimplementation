6bf4c6ab27e5a234dc9a04f8ce1f017e8a2104a0
walterlim <walterlim2001@yahoo.com>
2022-07-04 11:42:31 +0800

    added more emotes, show & hide emote panel, timer rework

--- Scenes/SpeechBubble.gd
+++ Scenes/SpeechBubble.gd
@@ -2,20 +2,28 @@ extends Sprite
 
 
 onready var emote_sprite = get_node("Emote")
+onready var timer = get_node("Timer")
 var check = preload("res://Assets/UI/Emotes/check.png")
 var clap = preload("res://Assets/UI/Emotes/clap.png")
 var cross = preload("res://Assets/UI/Emotes/cross.png")
+var heart = preload("res://Assets/UI/Emotes/heart.png")
+var ok = preload("res://Assets/UI/Emotes/ok.png")
+var party = preload("res://Assets/UI/Emotes/party.png")
+var raise = preload("res://Assets/UI/Emotes/raise.png")
+var thumbsup = preload("res://Assets/UI/Emotes/thumbsup.png")
 
 
-# Called when the node enters the scene tree for the first time.
 func _ready():
 	self.hide()
-	
+
+# shows emote by showing and hiding speech bubble with emote, uses a timer to time emote duration
 func startEmote():
 	self.show()
-	yield(get_tree().create_timer(5.0), "timeout")
+	timer.start(5)
+	yield(timer, "timeout")
 	self.hide()
 
+# each emote has a caller function together with preloaded texture
 func _on_Clap_pressed():
 	emote_sprite.set_texture(clap)
 	startEmote()
@@ -28,4 +36,29 @@ func _on_Yes_pressed():
 
 func _on_No_pressed():
 	emote_sprite.set_texture(cross)
+	startEmote()
+
+
+func _on_Ok_pressed():
+	emote_sprite.set_texture(ok)
+	startEmote()
+
+
+func _on_ThumbsUp_pressed():
+	emote_sprite.set_texture(thumbsup)
+	startEmote()
+
+
+func _on_Raise_pressed():
+	emote_sprite.set_texture(raise)
+	startEmote()
+
+
+func _on_Heart_pressed():
+	emote_sprite.set_texture(heart)
+	startEmote()
+
+
+func _on_Party_pressed():
+	emote_sprite.set_texture(party)
 	startEmote()
