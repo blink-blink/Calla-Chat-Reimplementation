771c3c6c9456e9f863fa72a56010b723d7a4969c
walterlim <walterlim2001@yahoo.com>
2022-07-07 12:22:31 +0800

    resources script added

---
+++ Scripts/Resources.gd
@@ -0,0 +1,13 @@
+extends Node
+
+var emotes = {}
+
+func _ready():
+	emotes["check"] = preload("res://Assets/UI/Emotes/check.png")
+	emotes["clap"] = preload("res://Assets/UI/Emotes/clap.png")
+	emotes["cross"] = preload("res://Assets/UI/Emotes/cross.png")
+	emotes["heart"] = preload("res://Assets/UI/Emotes/heart.png")
+	emotes["ok"] = preload("res://Assets/UI/Emotes/ok.png")
+	emotes["party"] = preload("res://Assets/UI/Emotes/party.png")
+	emotes["raise"] = preload("res://Assets/UI/Emotes/raise.png")
+	emotes["thumbsup"] = preload("res://Assets/UI/Emotes/thumbsup.png")
