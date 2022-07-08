6463d3aa8ebb95a3c62cf1feacc24f0a10529789
walterlim <walterlim2001@yahoo.com>
2022-07-08 18:44:55 +0800

    added signatures acc to protocol

--- Scripts/Multiplayer.gd
+++ Scripts/Multiplayer.gd
@@ -5,30 +5,45 @@ var mainplayer
 var usernames: Dictionary = {}
 var serverIP = "192.168.195.1"
 var serverPort = 22552
+var uniqueID: int
 
 func _ready():
+	get_tree().connect("connected_to_server", self, "connection_success")
+	get_tree().connect("connection_failed", self, "connection_failure")
+	get_tree().connect("server_disconnected", self, "disconnected")
 	pass
 
 func start_client():
 	var peer = NetworkedMultiplayerENet.new()
 	peer.create_client(serverIP, serverPort)
 	get_tree().network_peer = peer
+	
+func connection_success():
+	uniqueID = get_tree().get_network_unique_id()
 
-func spawn_new_player(id: int):
-	usernames[id] = get_username(id)
-	
-func despawn_player(id: int):
-	usernames.erase(id)
-	
-func get_username(id: int):
+func connection_failure():
 	pass
 	
-remote func set_all_user_positions(user_positions: Dictionary):
+func disconnected():
+	pass
+	
+func update_player_position(x: float, y: float):
+	rpc_unreliable("update_client_position", uniqueID, x, y)
+
+func send_emote(emoteint: int):
+	rpc("set_client_emote", uniqueID, emoteint)
+
+remote func set_all_user_positions(positions: Dictionary):
 	pass
 
-remote func disconnect_me(id: int):
-	emit_signal("network_peer_disconnected",id)
+remote func set_all_usernames(usernames: Dictionary):
 	pass
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-#func _process(delta):
-#	pass
+
+remote func set_all_avatars(avatars: Dictionary):
+	pass
+
+remote func add_new_user(userid: int, username: String, avatar: int):
+	pass
+
+remote func set_user_emote(userid: int, emote: int):
+	pass
