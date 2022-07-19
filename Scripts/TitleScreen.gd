418ce1acb15c161cf20b17140a5e4d840c6cb00d
walterlim <walterlim2001@yahoo.com>
2022-07-19 18:13:45 +0800

    added conf parsing

--- Scripts/TitleScreen.gd
+++ Scripts/TitleScreen.gd
@@ -2,7 +2,10 @@ extends Control
 
 onready var usernamefield = get_node("UserName")
 onready var avatarfield = get_node("Avatar")
-
+onready var filedialog = get_node("FileDialog")
+var password
+var address
+var usernumber
 # function for when join room button is pressed
 func _on_JoinRoom_pressed():
 	var username
@@ -13,3 +16,22 @@ func _on_JoinRoom_pressed():
 		username = usernamefield.text
 	Multiplayer.start_client(username, avatar)
 	
+
+
+func _on_ChooseConfig_pressed():
+	filedialog.popup_centered_clamped()
+	
+
+
+func _on_FileDialog_file_selected(path:String):
+	if path.get_extension() != "conf":
+		print("invalid conf file")
+		return
+	var file = File.new()
+	file.open(path, File.READ)
+	var content = file.get_as_text()
+	var splitlines = content.split("\n")
+	# Hardcoded config parsing
+	password = splitlines[0].substr(10).strip_edges()
+	address = splitlines[2].substr(10).strip_edges()
+	usernumber = str(int(address.trim_suffix("/24").split(".")[3])+198)
