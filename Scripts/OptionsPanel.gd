c3b63fa03a635b2ba7a181f0c7feb72abaf064e1
walterlim <walterlim2001@yahoo.com>
2022-07-28 15:08:15 +0800

    fixed volume slider

--- Scripts/OptionsPanel.gd
+++ Scripts/OptionsPanel.gd
@@ -5,12 +5,9 @@ onready var volumeslide = get_node("VBoxContainer/VolumeSlider")
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	volumeslide.value = GlobalAudioStreamPlayer.volume_db
-
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-#func _process(delta):
-#	pass
+	volumeslide.value = db2linear(GlobalAudioStreamPlayer.volume_db)
+	var valuelabel = volumeslide.get_child(1)
+	valuelabel.text = str(volumeslide.value)
 
 
 func _on_Button_pressed():
@@ -19,6 +16,6 @@ func _on_Button_pressed():
 
 func _on_HSlider_value_changed(value):
 	#volumeslider signal
-	GlobalAudioStreamPlayer.volume_db = value
+	GlobalAudioStreamPlayer.volume_db = linear2db(value)
 	var valuelabel = volumeslide.get_child(1)
 	valuelabel.text = str(value)
